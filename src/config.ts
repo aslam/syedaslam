b9d891387d17c864868a052c6def2564b162fdb1
Syed Aslam <aslam.maqsood@gmail.com>
2025-08-30 17:56:20 +0530

    Removed sidebarActiveId

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,7 @@ export const KNOWN_LANGUAGES = {
 
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/aslam/aslam`;
+export const GITHUB_EDIT_URL = `https://github.com/aslam/syedaslam`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
